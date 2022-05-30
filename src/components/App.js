2bd58dcc9b6c801342b6b4a61d70473866438eb6
vovitolog <vovitolog@gmail.com>
2022-05-30 19:36:30 +0300

    добавить получение данных пользователя из API

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
 import PopupWithForm from "./PopupWithForm";
+import Api from "../utils/Api"
 
 function App() {
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] =
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,31 @@
 import React from "react";
-import profilePhoto from "../images/profile_image.jpg";
+import profilePhoto from "../images/profile_image.jpg"; /// Убраьб,
 import photoChange from "../images/edit_button.svg";
+import api from "../utils/Api";
 
-function Main({onEditProfile, onAddPlace, onEditAvatar}) {
+function Main({ onEditProfile, onAddPlace, onEditAvatar }) {
+  const [userName, setUserName] = React.useState(0);
+  const [userDescription, setUserDescription] = React.useState(0);
+  const [userAvatar, setUserAvatar] = React.useState(0);
+
+  React.useEffect(() => {
+    api.getUserInfo().then((data) => {
+      setUserName(data.name);
+      setUserDescription(data.about);
+      setUserAvatar(data.avatar)
+    });
+  });
+
   return (
     <main className="content">
       <section className="profile">
         <div className="profile__wrapper">
-          <div
-            className="profile__photo-wrapper"
-            onClick={onEditAvatar}
-          >
+          <div className="profile__photo-wrapper" onClick={onEditAvatar}>
             <img
               className="profile__photo"
               alt="Фото профиля"
-              src={profilePhoto}
+              src={userAvatar}
+              //style={{ backgroundImage: `url(${userAvatar})` }} 
             />
             <img
               className="profile__photo-edit"
@@ -24,19 +35,16 @@ function Main({onEditProfile, onAddPlace, onEditAvatar}) {
           </div>
           <div className="profile__info">
             <div className="profile__name-wrapper">
-              <h1 className="profile__name">Жак-Ив Кусто</h1>
+              <h1 className="profile__name">{userName}</h1>
               <button
                 onClick={onEditProfile}
                 className="profile__button-edit"
               ></button>
             </div>
-            <p className="profile__profession">Исследователь океана</p>
+            <p className="profile__profession">{userDescription}</p>
           </div>
         </div>
-        <button
-          onClick={onAddPlace}
-          className="profile__button-add"
-        ></button>
+        <button onClick={onAddPlace} className="profile__button-add"></button>
       </section>
       <section className="cards">
         <ul className="cards__list"></ul>
