901ee4fae400bbf2f35c51b540eddbd6c4e08ab5
vovitolog <vovitolog@gmail.com>
2022-06-04 18:27:36 +0300

    feat: убрать иконку удаления карточки

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
-function Card({ card, onCardClick }) {
+function Card({ card, onCardClick, onCardLike }) {
+  const currentUser = React.useContext(CurrentUserContext);
+  const isOwn = card.owner._id === currentUser._id;
+  
+  const cardDeleteButtonClassName = (
+     `${isOwn ? 'card__button-delete' : ''}`
+  ); 
+
+  const isLiked = card.likes.some(i => i._id === currentUser._id);
+  //console.log(isLiked);
+
+// Создаём переменную, которую после зададим в `className` для кнопки лайка
+const cardLikeButtonClassName = (
+  `card__like ${isOwn ? 'card__like_visible' : 'card__like_hidden'}`
+); 
+
+
   function handleCardClick() {
     onCardClick(card);
+  }
+
+  function  handleLikeClick() {
+    onCardLike(card);
   }
 
   return (
@@ -13,11 +34,11 @@ function Card({ card, onCardClick }) {
         src={card.link}
         onClick={handleCardClick}
       />
-      <button className="card__button-delete"></button>
+      <button className={cardDeleteButtonClassName}></button>
       <div className="card__description">
         <h2 className="card__title">{card.name}</h2>
         <div className="card__like-wrapper">
-          <button className="card__like"></button>
+          <button className="card__like" onClick={handleLikeClick}></button>
           <p className="card__likes-count">{card.likes.length}</p>
         </div>
       </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -6,27 +6,24 @@ import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 function Main({ onEditProfile, onAddPlace, onEditAvatar, onCardClick }) {
 
-  
-/*   const [userName, setUserName] = React.useState('');
-  const [userDescription, setUserDescription] = React.useState('');
-  const [userAvatar, setUserAvatar] = React.useState(''); */
   const [cards, setCards] = React.useState([]);
 
   const currentUser = React.useContext(CurrentUserContext);
- 
-/*   React.useEffect(() => {
-    api
-      .getUserInfo()
-      .then((data) => {
-        setUserName(data.name);
-        setUserDescription(data.about);
-        setUserAvatar(data.avatar);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []); */
 
+
+  function handleCardLike(card) {
+    console.log('click');
+    // Снова проверяем, есть ли уже лайк на этой карточке
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    
+    // Отправляем запрос в API и получаем обновлённые данные карточки
+    api.changeLikeCardStatus(card._id, !isLiked).then((newCard) => {
+        setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
+    });
+  } 
+// catch error????
+
+ 
   React.useEffect(() => {
     api
       .getInitialCards()
@@ -37,9 +34,6 @@ function Main({ onEditProfile, onAddPlace, onEditAvatar, onCardClick }) {
         console.log(error);
       });
   }, []);
-
-
-
 
   return (
     <main className="content">
@@ -73,7 +67,7 @@ function Main({ onEditProfile, onAddPlace, onEditAvatar, onCardClick }) {
       <section className="cards">
         <ul className="cards__list">
           {cards.map((card) => (
-            <Card card={card} key={card._id} onCardClick={onCardClick} />
+            <Card card={card} key={card._id} onCardClick={onCardClick} onCardLike={handleCardLike}/>
           ))}
         </ul>
       </section>
